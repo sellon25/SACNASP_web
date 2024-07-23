aea863811a0d13cd57742dd8ad6ec8bcaaf7daea
sellon25 <117732516+sellon25@users.noreply.github.com>
2024-07-23 13:38:27 +0200

    .

--- SACNASP/settings.py
+++ SACNASP/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n0_@#55!wj!pu-^9kjpx#+gvlapz34^6zqhpn@0!#vc3v%o!b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
